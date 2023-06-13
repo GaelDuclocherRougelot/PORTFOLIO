a6854a286a37e0facc0cce7ef1ba84d7a1838daa
GaelDuclocherRougelot <gaelduclocher.rougelot@gmail.com>
2023-06-13 17:15:55 +0200

    ci: add title, picture

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,9 +1,35 @@
-import styles from './Banner.module.scss'
-
+import { motion } from "framer-motion";
+import styles from "./Banner.module.scss";
 export default function Banner() {
   return (
     <section className={`${styles.banner__section}`}>
-
+      <div className={`${styles.title__container}`}>
+        <motion.h3
+          initial={{ x: -20, opacity: 0 }}
+          animate={{ x: 0, opacity: 1 }}
+          transition={{ duration: 0.5, delay: 0.5, ease: "easeInOut" }}
+        >
+          Welcome
+        </motion.h3>
+        <motion.h1
+          initial={{ x: -20, opacity: 0 }}
+          animate={{ x: 0, opacity: 1 }}
+          transition={{ duration: 0.5, delay: 0.7, ease: "easeInOut" }}
+        >
+          Hey ! I&#39;m Gaël ,
+        </motion.h1>
+        <motion.h2
+          initial={{ x: -20, opacity: 0 }}
+          animate={{ x: 0, opacity: 1 }}
+          transition={{ duration: 0.5, delay: 0.9, ease: "easeInOut" }}
+        >
+          <span className={`${styles.js__span}`}>Javascript</span> Developer.
+        </motion.h2>
+      </div>
+      <motion.img 
+          initial={{ x: -20, opacity: 0 }}
+          animate={{ x: 0, opacity: 1 }}
+          transition={{ duration: 0.5, delay: 1.1, ease: "easeInOut" }} src="/profile-pic.png" alt="profile picture" />
     </section>
-  )
+  );
 }
